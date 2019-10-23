18990ac821dc8dd487353cad96967540b101547c
Samir Omarov <samir.omarov@gmail.com>
2019-10-23 23:28:32 +0300

    Add Post list & Search function

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,16 +1,39 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from .models import Post
 from marketing.models import Newsletter
+from django.db.models import Count, Q
+
+def search(request):
+    queryset = Post.objects.all()
+    query = request.GET.get('q')
+    
+    if query:
+        queryset = queryset.filter(
+            Q(title__icontains=query) |
+            Q(overview__icontains=query)
+        ).distinct()
+    context = {
+        'queryset' : queryset,
+    }
+
+    return render(request, 'search_results.html', context)
+
+def get_category_count():
+    queryset = Post.objects.values('categories__title').annotate(Count('categories__title'))
+    
+    return queryset
+
 
 def index(request):
     featured = Post.objects.filter(featured = True)
     latest = Post.objects.order_by('-timestamp')[0:3]
-
     if request.method == "POST":
         email = request.POST["email"]
         new_newsletter = Newsletter()
         new_newsletter.email = email
         new_newsletter.save()
+        
     context = {
         'object_list': featured,
         'latest': latest
@@ -18,7 +41,28 @@ def index(request):
     return render(request, 'index.html',  context)
 
 def blog(request):
-    return render(request, 'blog.html', {})
+    category_count = get_category_count()
+    most_recent = Post.objects.order_by('-timestamp')[:3]
+    post_list = Post.objects.all()
+    paginator  = Paginator(post_list, 4)
+    page_request_var = 'page'
+    page = request.GET.get(page_request_var)
+    
+    try:
+        paginated_queryset = paginator.page(page)
+    except PageNotAnInteger:
+        paginated_queryset  = paginator.page(1) 
+    except EmptyPage:
+        paginated_queryset = paginator.page(paginator.num_pages)
 
-def post(request):
+    context = {
+        'queryset' : paginated_queryset,
+        'most_recent' : most_recent,
+        'page_request_var' : page_request_var,
+        'category_count' : category_count,
+    }
+
+    return render(request, 'blog.html', context)
+
+def post(request, id):
     return render(request, 'post.html', {})
