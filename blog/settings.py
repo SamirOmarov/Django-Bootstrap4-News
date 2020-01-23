95b5a8eba71f9fe88f8349eece80d6897f9f07c8
Samir Omarov <samir.omarov@gmail.com>
2020-01-23 15:53:55 +0300

    Add gitattributes

--- blog/settings.py
+++ blog/settings.py
@@ -4,8 +4,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'xf0-hs)2kf7rjk1pf=#e4h!)uj%i*(5s74)zw=c5ha1fw+)ou2'
@@ -30,6 +28,7 @@ INSTALLED_APPS = [
     # Apps
     'posts',
     'marketing',
+    
     #Third Party:
     'tinymce',
     'crispy_forms',
